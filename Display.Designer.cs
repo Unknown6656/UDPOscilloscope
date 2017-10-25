821b6083ee3fc0507b12a457441c21a07fafac66
Unknown6656 <unknown84@gmx.de>
2017-10-25 14:43:28 +0200

    initial code commit

---
+++ Display.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace UDPOscilloscope
+{
+    partial class Display
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.scottPlotUC1 = new ScottPlot.ScottPlotUC();
+            this.SuspendLayout();
+            // 
+            // scottPlotUC1
+            // 
+            this.scottPlotUC1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scottPlotUC1.Location = new System.Drawing.Point(0, 0);
+            this.scottPlotUC1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.scottPlotUC1.Name = "scottPlotUC1";
+            this.scottPlotUC1.Size = new System.Drawing.Size(910, 602);
+            this.scottPlotUC1.TabIndex = 0;
+            // 
+            // Display
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 602);
+            this.Controls.Add(this.scottPlotUC1);
+            this.Name = "Display";
+            this.Text = "Display";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal ScottPlot.ScottPlotUC scottPlotUC1;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UDPOscilloscope
+{
+    public static class Program
+    {
+        private const int listenPort = 31488;
+        private static readonly Queue<byte[]> queue = new Queue<byte[]>();
+
+
+        public static int Main(string[] argv)
+        {
+            using (Display disp = new Display())
+            using (UdpClient listener = new UdpClient(listenPort))
+            {
+                IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenPort);
+                byte[] buffer;
+                bool done = false;
+
+                disp.Load += delegate
+                {
+                    new Task(async () =>
+                    {
+                        while (!done)
+                        {
+                            if (queue.Count > 0)
+                            {
+                                byte[] frame = queue.Dequeue();
+                                double[] dY = new double[frame.Length / 2];
+                                double[] dX = new double[dY.Length];
+
+                                Parallel.For(0, dY.Length, i => (dY[i], dX[i]) = ((frame[i * 2] << 8) | frame[i * 2 + 1], i));
+
+                                disp.Invoke(new MethodInvoker(() =>
+                                {
+                                    disp.scottPlotUC1.Xs = dX;
+                                    disp.scottPlotUC1.Ys = dY;
+                                    disp.scottPlotUC1.UpdateGraph();
+
+                                    Application.DoEvents();
+
+                                    disp.Update();
+                                }));
+                            }
+                            else
+                                await Task.Delay(50);
+
+                            Application.DoEvents();
+                        }
+                    }).Start();
+                };
+                disp.FormClosing += (s, e) => done = true;
+                disp.Show();
+
+                try
+                {
+                    while (!done)
+                    {
+                        Console.WriteLine("Waiting for broadcast ...");
+
+                        buffer = listener.Receive(ref groupEP);
+
+                        Console.WriteLine($"Received a broadcast from {groupEP} with a length of {buffer.Length / 1024f:F1}kB");
+
+                        queue.Enqueue(buffer);
+                    }
+                }
+                catch (Exception e)
+                {
+                    StringBuilder sb = new StringBuilder();
+
+                    while (e != null)
+                    {
+                        sb.Insert(0, e.Message + e.StackTrace);
+
+                        e = e.InnerException;
+                    }
+
+                    Console.WriteLine(sb.ToString());
+                }
+
+                
+            }
+
+            return 0;
+        }
+    }
+}
